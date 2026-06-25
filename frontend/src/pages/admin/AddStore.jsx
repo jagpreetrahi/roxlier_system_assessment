bae5229969b854a90c3426cf1db3bf9aacd3d84a
jagpreetrahi <jagpreet1615@gmail.com>
2026-06-25 23:39:31 +0530

    Fix the importing path

--- frontend/src/pages/admin/AddStore.jsx
+++ frontend/src/pages/admin/AddStore.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import Navbar from '../../components/Navbar';
+import Navbar from '../../components/NavBar';
 import API from '../../api/axios';
 
 
